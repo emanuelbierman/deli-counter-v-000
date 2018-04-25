6f9adcdff183f4799d212202746760af8adc3639
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-25 19:44:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(katz_deli)
   else
     line_array = []
     katz_deli.each_with_index do |element, index|
-      line_array << katz_deli[index].insert(0,"#{index}. ")
+      line_array << katz_deli[index].insert(0,"#{index + 1}. ")
     end
     puts "The line is currently: #{line_array.join}"
   end
