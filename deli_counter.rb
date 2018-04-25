ec6c627ce4561508af2e0daff24c4d11902ace1e
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-25 19:10:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,13 @@ def take_a_number(katz_deli, name)
 end
 
 def line(katz_deli)
-  
+
 end
 
 def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0].}"
+    katz_deli.shift[0]
 end
